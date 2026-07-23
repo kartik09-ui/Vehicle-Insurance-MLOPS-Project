4f30d0d9bfd21c1aa7128ee0a052f0225d6f15c1
Kartik Malik <kartikmalik2796@gmail.com>
2026-07-23 18:12:43 +0530

    project structure ready, VENV set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version = '0.0.1',
+    author = 'Kartik Malik',
+    author_email = 'kartikmalik2796@gmail.com',
+    packages = find_packages()
+)
