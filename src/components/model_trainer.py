332426baf4abab6118f2ef3a7734bd219f049d1d
Kartik Malik <kartikmalik2796@gmail.com>
2026-07-28 15:13:13 +0530

    complete till model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -56,7 +56,7 @@ class ModelTrainer:
             accuracy = accuracy_score(y_test, y_pred)
             f1 = f1_score(y_test, y_pred)
             precision = precision_score(y_test, y_pred)
-            recall = recall(y_test, y_pred) 
+            recall = recall_score(y_test, y_pred) 
 
             # Ceating metric artifact 
             metric_artifact = ClassificationMetricArtifact(f1_score=f1, precision_score=precision,recall_score=recall)
